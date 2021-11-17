4f926d47c76157d2cd81392e76129ab3dc8cee50
Justin Taylor <justin2taylor@gmail.com>
2021-11-17 09:38:46 -0800

    init from vite starter

---
+++ src/jsx/anim.ts
@@ -0,0 +1,5 @@
+/// <reference path="../../node_modules/types-for-adobe/Animate/2013/index.d.ts" />
+
+export const helloWorld = () => {
+  alert('Hello from Animate');
+};
---
+++ src/jsx/ilst.ts
@@ -0,0 +1,5 @@
+/// <reference path="../../node_modules/types-for-adobe/Illustrator/2015.3/index.d.ts" />
+
+export const helloWorld = () => {
+  alert('Hello from Illustrator');
+};
