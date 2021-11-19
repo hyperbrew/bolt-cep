8363791562975a4cc13e17c3fbe6cc049cadd92c
Justin Taylor <justin2taylor@gmail.com>
2021-11-18 21:34:22 -0800

    solved JS config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import babel from '@rollup/plugin-babel'
+import babel from "@rollup/plugin-babel";
+import usePluginImport from "vite-plugin-importer";
+
+import builtins from "rollup-plugin-node-builtins";
+// import replace from '@rollup/plugin-replace';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import copyNode from "rollup-plugin-node-copy";
+
+import commonjs from "@rollup/plugin-commonjs";
+
+import scss from "rollup-plugin-scss";
+import image from "@rollup/plugin-image";
 
 import { cep, jsxInclude, customFunction } from "./rollup-cep-plugin/index.js";
 import cepConfig from "./cep.config.json";
 
-const extensions = ['.js', '.ts', '.tsx'];
+const extensions = [".js", ".ts", ".tsx"];
 
 const cepVars = {
   devDist: "dist",
@@ -20,7 +31,50 @@ const cepVars = {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      // babel: {
+      //   // exclude: /node_modules/,
+      //   babelrc: false,
+      //   // babelHelpers: "runtime",
+      //   presets: [
+      //     [
+      //       "env",
+      //       {
+      //         targets: {
+      //           chrome: "80",
+      //         },
+      //       },
+      //     ],
+      //     // [
+      //     //   "@babel/preset-env",
+      //     //   {
+      //     //     useBuiltIns: "entry",
+      //     //   },
+      //     // ],
+      //     "@babel/preset-react",
+      //     "@babel/preset-typescript",
+      //   ],
+      //   plugins: [
+      //     // "react-require",
+      //     //   "@babel/plugin-syntax-dynamic-import",
+      //     //   "@babel/plugin-proposal-class-properties",
+      //     //   [
+      //     //     "@babel/plugin-proposal-object-rest-spread",
+      //     //     {
+      //     //       useBuiltIns: true,
+      //     //     },
+      //     //   ],
+      //     //   [
+      //     //     "@babel/plugin-transform-runtime",
+      //     //     {
+      //     //       helpers: true,
+      //     //       regenerator: true,
+      //     //       useESModules: false,
+      //     //     },
+      //     //   ],
+      //   ],
+      // },
+    }),
     cep({
       config: cepConfig,
       dir: `${__dirname}/${cepVars.devDist}`,
@@ -32,34 +86,20 @@ export default defineConfig({
       isLocal: cepVars.isLocal,
       debugReact: cepVars.debugReact,
     }),
-      babel({
-          extensions,
-          exclude: /node_modules/,
-          babelrc: false,
-          babelHelpers: 'runtime',
-          presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-          plugins: [
-            'react-require',
-            '@babel/plugin-syntax-dynamic-import',
-            '@babel/plugin-proposal-class-properties',
-            [
-              '@babel/plugin-proposal-object-rest-spread',
-              {
-                useBuiltIns: true,
-              },
-            ],
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                helpers: true,
-                regenerator: true,
-                useESModules: false,
-              },
-            ],
-          ],
-        }),
   ],
   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        // esModule: false,
+        // preserveModules: false,
+        format: "iife",
+      },
+    },
+    target: "chrome88",
     outDir: "dist/cep",
   },
 });
+
+// TODO: Fix paths from / to ./ in html and js
+// TODO: Move Script tag from head to body after <div>
