21cd2959800877868351f3199779119e9635e29f
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2023-10-21 13:39:20 -0700

    add `ame`, `audt`, `idsn`, `kbrg` folders

--- src/jsx/index.ts
+++ src/jsx/index.ts
@@ -3,30 +3,58 @@
 import { ns } from "../shared/shared";
 
 import * as aeft from "./aeft/aeft";
-import * as ilst from "./ilst/ilst";
+import * as ame from "./ame/ame";
 import * as anim from "./anim/anim";
-import * as ppro from "./ppro/ppro";
+import * as audt from "./audt/audt";
+import * as idsn from "./idsn/idsn";
+import * as ilst from "./ilst/ilst";
+import * as kbrg from "./kbrg/kbrg";
 import * as phxs from "./phxs/phxs";
+import * as ppro from "./ppro/ppro";
 
 let main: any;
 
-switch (BridgeTalk.appName) {
-  case "premierepro":
-  case "premiereprobeta":
-    main = ppro;
-    break;
+switch (BridgeTalk.appName as ApplicationName) {
   case "aftereffects":
   case "aftereffectsbeta":
     main = aeft;
     break;
+
+  case "ame":
+  case "amebeta":
+    main = ame;
+    break;
+
+  case "audition":
+  case "auditionbeta":
+    main = audt;
+    break;
+
+  case "bridge":
+  case "bridgebeta":
+    main = kbrg;
+    break;
+
   case "illustrator":
   case "illustratorbeta":
     main = ilst;
     break;
+
+  case "indesign":
+  case "indesignbeta":
+    main = idsn;
+    break;
+
   case "photoshop":
   case "photoshopbeta":
     main = phxs;
     break;
+
+  case "premierepro":
+  case "premiereprobeta":
+    main = ppro;
+    break;
+
   default:
     //@ts-ignore
     if (app.appName === "Adobe Animate") {
@@ -34,12 +62,38 @@ switch (BridgeTalk.appName) {
     }
     break;
 }
+
 //@ts-ignore
 const host = typeof $ !== "undefined" ? $ : window;
 host[ns] = main;
 
 export type Scripts = typeof aeft &
-  typeof ilst &
+  typeof ame &
   typeof anim &
-  typeof ppro &
-  typeof phxs;
+  typeof audt &
+  typeof idsn &
+  typeof ilst &
+  typeof kbrg &
+  typeof phxs &
+  typeof ppro;
+
+// https://extendscript.docsforadobe.dev/interapplication-communication/bridgetalk-class.html?highlight=bridgetalk#appname
+type ApplicationName =
+  | "aftereffects"
+  | "aftereffectsbeta"
+  | "ame"
+  | "amebeta"
+  | "audition"
+  | "auditionbeta"
+  | "bridge"
+  | "bridgebeta"
+  // | "flash"
+  | "illustrator"
+  | "illustratorbeta"
+  | "indesign"
+  | "indesignbeta"
+  // | "indesignserver"
+  | "photoshop"
+  | "photoshopbeta"
+  | "premierepro"
+  | "premiereprobeta";
