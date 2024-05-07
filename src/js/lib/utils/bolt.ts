428a57380a6b453700f358f4de88d1d275e608e1
Justin Taylor <justin@justintaylor.tv>
2024-05-07 10:17:23 -0700

    attempt showOpenDialogEx

--- src/js/lib/utils/bolt.ts
+++ src/js/lib/utils/bolt.ts
@@ -299,12 +299,9 @@ export const selectFolder = (
   msg: string,
   callback: (res: string) => void
 ) => {
-  const result = window.cep.fs.showOpenDialog(
-    false,
-    true,
-    msg,
-    dir
-  ) as IOpenDialogResult;
+  const result = (
+    window.cep.fs.showOpenDialogEx || window.cep.fs.showOpenDialog
+  )(false, true, msg, dir) as IOpenDialogResult;
   if (result.data?.length > 0) {
     const folder = decodeURIComponent(result.data[0].replace("file://", ""));
     callback(folder);
@@ -316,12 +313,9 @@ export const selectFile = (
   msg: string,
   callback: (res: string) => void
 ) => {
-  const result = window.cep.fs.showOpenDialog(
-    false,
-    false,
-    msg,
-    dir
-  ) as IOpenDialogResult;
+  const result = (
+    window.cep.fs.showOpenDialogEx || window.cep.fs.showOpenDialog
+  )(false, false, msg, dir) as IOpenDialogResult;
   if (result.data?.length > 0) {
     const folder = decodeURIComponent(result.data[0].replace("file://", ""));
     callback(folder);
