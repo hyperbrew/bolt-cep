a8288fa1e9a55ab0f8e885daaf9f5d2a1dbcc1c2
Justin Taylor <justin@justintaylor.tv>
2025-03-10 12:06:18 -0700

    remove ppro metadata

--- src/jsx/ppro/ppro-utils.ts
+++ src/jsx/ppro/ppro-utils.ts
@@ -254,6 +254,27 @@ export const setPrMetadata = (
   projectItem.setProjectMetadata(str, array);
 };
 
+export const removePrMetadata = (
+  projectItem: ProjectItem,
+  fields: string[]
+) => {
+  let PProMetaURI = "http://ns.adobe.com/premierePrivateProjectMetaData/1.0/";
+  if (ExternalObject.AdobeXMPScript === undefined) {
+    ExternalObject.AdobeXMPScript = new ExternalObject("lib:AdobeXMPScript");
+  }
+  if (!app.isDocumentOpen() || !ExternalObject.AdobeXMPScript || !XMPMeta) {
+    return {};
+  }
+  let xmp = new XMPMeta(projectItem.getProjectMetadata());
+  var array = [];
+  for (var i = 0; i < fields.length; i++) {
+    xmp.deleteProperty(PProMetaURI, fields[i]);
+    array.push(fields[i]);
+  }
+  var str = xmp.serialize();
+  projectItem.setProjectMetadata(str, array);
+};
+
 // Motion Graphics Template ( MOGRT ) Helpers
 
 export const fillMogrtText = (
